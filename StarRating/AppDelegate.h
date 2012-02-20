0dcf41cb7c6b9b8ee2a2ce7ffdba17735bd0ef6a
Benny Sheerin <bennysheerin@yahoo.ie>
2012-02-20 13:18:12 +1100

    Initial Commit

---
+++ StarRating/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StarRating
+//
+//  Created by Benny Sheerin on 20/02/12.
+//  Copyright (c) 2012 bennythemink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StarRating/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StarRating
+//
+//  Created by Benny Sheerin on 20/02/12.
+//  Copyright (c) 2012 bennythemink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
